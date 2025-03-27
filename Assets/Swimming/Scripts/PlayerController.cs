f25829ad094623b0ac5c8dffdb65c95f9b7964e8
Helena Bjeletich <98721667+hbjeletich@users.noreply.github.com>
2025-03-26 23:57:11 -0500

    Refined movement & camera

--- Assets/Swimming/Scripts/PlayerController.cs
+++ Assets/Swimming/Scripts/PlayerController.cs
@@ -3,28 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace Swimming {
+namespace Swimming
+{
     public class PlayerController : MonoBehaviour
     {
         // serialized values
         [SerializeField] private InputActionAsset inputActions;
         [SerializeField] private float moveSpeed = 3f;
+        [SerializeField] private float swimForce = 20f;
+        [SerializeField] private float maxSwimVelocity = 4f;
+        [SerializeField] private float waterDrag = 1.5f;
+        private bool isSwimming = false;
 
-        // input actons
+        // debug mode for using keyboard input
+        [SerializeField] private bool debugMode = false; 
+
+        // input actions
         private InputAction weightShiftLeftAction;
         private InputAction weightShiftRightAction;
         private InputAction weightShiftXAction;
+        private InputAction raiseFootAction;
+        private InputAction lowerFootAction;
+
+        // debug input actions
+        [SerializeField] private InputActionAsset debugActions;
+        private InputAction moveAction;
+        private InputAction jumpAction;
 
         private Rigidbody2D rigidbody2D;
 
         private void Awake()
-        {
+        {        
             rigidbody2D = GetComponent<Rigidbody2D>();
+            rigidbody2D.drag = waterDrag; // for underwater feel
 
-            var actionMap = inputActions.FindActionMap("MotionTracking");
-            weightShiftLeftAction = actionMap.FindAction("WeightShiftLeft");
-            weightShiftRightAction = actionMap.FindAction("WeightShiftRight");
-            weightShiftXAction = actionMap.FindAction("WeightShiftX");
+            // set up motion tracking actions
+            var motionMap = inputActions.FindActionMap("MotionTracking");
+            weightShiftLeftAction = motionMap.FindAction("WeightShiftLeft");
+            weightShiftRightAction = motionMap.FindAction("WeightShiftRight");
+            weightShiftXAction = motionMap.FindAction("WeightShiftX");
+            raiseFootAction = motionMap.FindAction("FootRaise");
+            lowerFootAction = motionMap.FindAction("FootLower");
+
+            // set up keyboard input for debug mode
+            var playerMap = debugActions.FindActionMap("SwimmingDebug");
+
+            if (playerMap == null) Debug.LogError("player map not found");
+            moveAction = playerMap.FindAction("Move");
+            jumpAction = playerMap.FindAction("Jump");
         }
 
         private void OnEnable()
@@ -32,6 +58,16 @@ namespace Swimming {
             weightShiftLeftAction.Enable();
             weightShiftRightAction.Enable();
             weightShiftXAction.Enable();
+
+            moveAction.Enable();
+            jumpAction.Enable();
+
+            // upward movement callbacks
+            raiseFootAction.performed += _ => StartSwimming();
+            lowerFootAction.performed += _ => StopSwimming();
+
+            jumpAction.started += _ => StartSwimming();
+            jumpAction.canceled += _ => StopSwimming();
         }
 
         private void OnDisable()
@@ -39,15 +75,61 @@ namespace Swimming {
             weightShiftLeftAction.Disable();
             weightShiftRightAction.Disable();
             weightShiftXAction.Disable();
+
+            moveAction.Disable();
+            jumpAction.Disable();
+
+            // upward movement
+            raiseFootAction.performed -= _ => StartSwimming();
+            lowerFootAction.performed -= _ => StopSwimming();
+
+            jumpAction.started -= _ => StartSwimming();
+            jumpAction.canceled -= _ => StopSwimming();
         }
 
-        private void Update()
+        private void FixedUpdate()
         {
-            // get foot height value and use it for horizontal movement
-            float weightShift = weightShiftXAction.ReadValue<float>();
+            if (debugMode)
+            {
+                // keyboard input in debug mode
+                float horizontalInput = moveAction.ReadValue<Vector2>().x;
+                DoHorizontalMovement(horizontalInput);
+            }
+            else
+            {
+                // motion tracking input when not in debug mode
+                float weightShift = weightShiftXAction.ReadValue<float>();
+                DoHorizontalMovement(weightShift);
+            }
 
-            rigidbody2D.velocity = new Vector2(weightShift * moveSpeed, 0);
+            if (isSwimming)
+            {
+                DoSwimming();
+            }
+        }
+
+        private void DoHorizontalMovement(float _input)
+        {
+            float verticalVelocity = rigidbody2D.velocity.y;
+            rigidbody2D.velocity = new Vector2(_input * moveSpeed, verticalVelocity);
+        }
+
+        private void DoSwimming()
+        {
+            if (rigidbody2D.velocity.y < maxSwimVelocity)
+            {
+                rigidbody2D.AddForce(Vector2.up * swimForce, ForceMode2D.Force);
+            }
+        }
+
+        private void StartSwimming()
+        {
+            isSwimming = true;
+        }
+
+        private void StopSwimming()
+        {
+            isSwimming = false;
         }
     }
-
 }
