e55e681106f35e42d65d53f6ec3034981642d16e
hbjeletich <helena@bjeletich.org>
2025-07-21 15:55:26 -0500

    golf - added ticking in wwise

--- Assets/Golf/Scripts/SwingStrengthController.cs
+++ Assets/Golf/Scripts/SwingStrengthController.cs
@@ -14,6 +14,11 @@ public class SwingStrengthController : MonoBehaviour
     private float barRate = 5f;
     private float strengthMultiplier = 1.2f;
 
+    // wwise audio integration
+    [SerializeField] private AK.Wwise.Event tickingEvent;
+    private uint tickingSoundID;
+    private bool isPlayingTick = false;
+
     // singleton set up
     public static SwingStrengthController instance { get; private set; }
 
@@ -36,6 +41,11 @@ public class SwingStrengthController : MonoBehaviour
     {
         swingStrength = Mathf.MoveTowards(swingStrength, targetStrength, barRate * Time.deltaTime);
         strengthMeter.value = swingStrength; //(Visualizing purposes)
+
+        if (isPlayingTick)
+        {
+            AkSoundEngine.SetRTPCValue("Swing_Hold_Time", swingStrength, gameObject);
+        }
     }
 
     public void toStartingState(){
@@ -44,12 +54,33 @@ public class SwingStrengthController : MonoBehaviour
         //slider for visualizing purpose.
         strengthMeter.value = swingStrength;
         strengthMeter.maxValue = maxStrength;
+
+        startTicking();
     }
 
     public void stopMeasuring(){
         //stops measuring strength of swing
         targetStrength = swingStrength; //stops the swing strength from increasing
+        stopTicking();
         Invoke("deactiveSelf", 1f);
+    }
+
+    private void startTicking()
+    {
+        if (!isPlayingTick)
+        {
+            tickingSoundID = tickingEvent.Post(gameObject);
+            isPlayingTick = true;
+        }
+    }
+
+    private void stopTicking()
+    {
+        if (isPlayingTick)
+        {
+            AkSoundEngine.StopPlayingID(tickingSoundID);
+            isPlayingTick = false;
+        }
     }
 
     public void deactiveSelf(){
