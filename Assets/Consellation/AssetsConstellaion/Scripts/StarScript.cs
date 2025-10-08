8962beb3ea0592e89aa5343912df22d425bd5dd1
tbsully0418 <149627439+tbsully0418@users.noreply.github.com>
2025-10-08 11:48:40 -0700

    star made nothing made

---
+++ Assets/Consellation/AssetsConstellaion/Scripts/StarScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace constellation
+{
+    public class StarScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            Destroy(gameObject);
+            print("Hit");
+        }
+    }
+}
