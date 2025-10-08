a2cea5fd7ba705eda5f858de8367196b5ad9f8ae
JJ-UT <149627462+JJ-UT@users.noreply.github.com>
2025-10-08 13:24:49 -0500

    Script Fix

--- Assets/Pottery/Scripts/Settings_Script.cs
+++ Assets/Pottery/Scripts/Settings_Script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using uintyEngine.SceneManagement;
+using UnityEngine.SceneManagement;
 
 public class Settings_Script : MonoBehaviour
 {
