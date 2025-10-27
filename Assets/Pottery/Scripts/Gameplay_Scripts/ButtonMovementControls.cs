68b87ede5b1fb4dbb6ddc62b0e59b767def7fc58
JJ-UT <149627462+JJ-UT@users.noreply.github.com>
2025-10-27 14:15:40 -0500

    Captury script updated

--- Assets/Pottery/Scripts/Gameplay_Scripts/ButtonMovementControls.cs
+++ Assets/Pottery/Scripts/Gameplay_Scripts/ButtonMovementControls.cs
@@ -9,20 +9,37 @@ public class ButtonMovementControls : MonoBehaviour
     public InputActionAsset inputActionAsset;
     private InputAction leftFootHeightAction;
     private InputAction leftHipAbducted;
+    private InputAction rightHipAbducted;
+    private InputAction leftFootRaised;
 
+    public GameObject mainMenuPanel;
     public GameObject settingsPanel;
+    public GameObject SelectionPanel;
 
     // Start is called before the first frame update
     void Awake()
     {
         var footMap = inputActionAsset.FindActionMap("Foot"); //plugin unity captury 
+
         if (footMap == null) Debug.LogWarning("Pottery: Foot map not found!");
         if (footMap != null) Debug.Log("Pottery: Foot map found!");
         //var footMap = inputActions.FindActionMap("Foot");
 
-        leftHipAbducted = footMap.FindAction("LeftHipAbducted");
+
+
         if (leftHipAbducted == null) Debug.LogWarning("Pottery: Left Hip Abducted not found!");
         if (leftHipAbducted != null) Debug.Log("Pottery: Left Hip Abducted found!");
+
+        if (rightHipAbducted == null) Debug.LogWarning("Pottery: right Hip Abducted not found!");
+        if (rightHipAbducted != null) Debug.Log("Pottery: right Hip Abducted found!");
+
+        if (leftFootRaised == null) Debug.LogWarning("Pottery: Left foot raised not found!");
+        if (leftFootRaised != null) Debug.Log("Pottery: left foot raised found!");
+
+
+        leftHipAbducted = footMap.FindAction("LeftHipAbducted");
+        rightHipAbducted = footMap.FindAction("RightHipAbducted");
+        leftFootRaised = footMap.FindAction("FootRaised");
     }
 
     void OnEnable()
@@ -30,6 +47,16 @@ public class ButtonMovementControls : MonoBehaviour
         leftHipAbducted.Enable();
         Debug.Log("Pottery: OnEnable");
         leftHipAbducted.performed += OnLeftHip;
+
+
+        rightHipAbducted.Enable();
+        Debug.Log("Pottery: OnEnable");
+        rightHipAbducted.performed += OnRightHip;
+
+        leftFootRaised.Enable();
+;       Debug.Log("Pottery; OnEnable");
+        leftFootRaised.performed += onLeftFoot;
+
     }
 
     void OnDisable()
@@ -39,11 +66,10 @@ public class ButtonMovementControls : MonoBehaviour
         leftHipAbducted.performed -= OnLeftHip;
     }
 
-    // Update is called once per frame
 
-    void Update()
+    public void ShowMainMenu()
     {
-
+        SetActivePanel(mainMenuPanel);
     }
 
     public void ShowSettings()
@@ -51,18 +77,49 @@ public class ButtonMovementControls : MonoBehaviour
         SetActivePanel(settingsPanel);
     }
 
+
+    public void ShowSelection()
+    {
+        SetActivePanel(SelectionPanel);
+    }
+
+
+
+    ////// Set active panel below////////
+
     private void SetActivePanel(GameObject activePanel)
     {
+
         settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+        SelectionPanel.SetActive(false);
+
 
         activePanel.SetActive(true);
     }
 
+
+
+    void OnRightHip(InputAction.CallbackContext context)
+    {
+
+        Debug.Log("Pottery: OnRightHip");
+        ShowMainMenu();
+
+    }
 
     void OnLeftHip(InputAction.CallbackContext context)
     {
         Debug.Log("Pottery: OnLeftHip");
         // whatever you write here happens when left hip abducted
         ShowSettings();
+    }
+
+
+    void onLeftFoot (InputAction.CallbackContext context)
+    {
+        Debug.Log("Left Foot raised");
+        ShowSelection();
+
     }
 }
