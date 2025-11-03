b992b2d71ba02021efe7b4544765d712c2ad0690
ArgyZu <gamesjovanna@gmail.com>
2025-11-03 08:46:30 -0600

    Finalized judge.cs

--- Assets/RhythmKitchen/ScriptsRhythmKitchen/GameplayScripts/RKJudge.cs
+++ Assets/RhythmKitchen/ScriptsRhythmKitchen/GameplayScripts/RKJudge.cs
@@ -10,7 +10,7 @@ namespace RhythmKitchen
     {
         [Header("Refs")]
         [SerializeField] private RKConductor conductor;
-        [SerializeField] private RKSpawner spawner;
+        [SerializeField] private Transform notesRuntime; // parent object for spawned notes
 
         [Header("Keys (A/W/S/D by lane)")]
         public KeyCode keyLane1 = KeyCode.A;
@@ -25,16 +25,82 @@ namespace RhythmKitchen
 
         void Update()
         {
-            if (conductor == null)
+            if (conductor == null || notesRuntime == null)
             {
                 return;
             }
-
+            if (Input.GetKeyDown(keyLane1))
+            {
+                TryHit(RKNote.Type.Lane1);
+            }
+            if (Input.GetKeyDown(keyLane2))
+            {
+                TryHit(RKNote.Type.Lane2);
+            }
+            if (Input.GetKeyDown(keyLane3))
+            {
+                TryHit(RKNote.Type.Lane3);
+            }
+            if (Input.GetKeyDown(keyLane4))
+            {
+                TryHit(RKNote.Type.Lane4);
+            }
         }
 
         private void TryHit(RKNote.Type type)
         {
-            
+            RKNote target = FindClosestNoteInLane(type);
+            if (target == null)
+            {
+                return;
+            }
+            float now = conductor.songTime;
+            float delta = Mathf.Abs(now - target.targetTime);
+
+            if (delta <= perfectWindow)
+            {
+                OnHit(target, "PERFECT");
+            }
+            else if (delta <= goodWindow)
+            {
+                OnHit(target, "GOOD");
+            }
+            else if (delta <= missWindow)
+            {
+                OnHit(target, "MISS");
+            }
+            else
+            {
+                // too far away
+            }
+        }
+
+        private RKNote FindClosestNoteInLane(RKNote.Type type)
+        {
+            RKNote closest = null;
+            float closestDelta = float.MaxValue;
+
+            foreach (Transform child in notesRuntime)
+            {
+                RKNote note = child.GetComponent<RKNote>();
+                if (note != null && note.noteType == type)
+                {
+                    float now = conductor.songTime;
+                    float delta = Mathf.Abs(now - note.targetTime);
+                    if (delta < closestDelta)
+                    {
+                        closestDelta = delta;
+                        closest = note;
+                    }
+                }
+            }
+            return closest;
+        }
+        private void OnHit(RKNote note, string rating)
+        {
+            // NOTE: expand this to add score, UI, SFX
+            Debug.Log($"[Judge] {rating} {note.noteType}");
+            Destroy(note.gameObject);
         }
     }
 }
