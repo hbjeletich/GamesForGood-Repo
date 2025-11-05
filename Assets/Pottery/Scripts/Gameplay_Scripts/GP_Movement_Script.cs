051f41d38c934a9b405da9a12bf31957c38d95f1
JJ-UT <149627462+JJ-UT@users.noreply.github.com>
2025-11-05 13:27:48 -0600

    Movement Fully implemented for gameplay

---
+++ Assets/Pottery/Scripts/Gameplay_Scripts/GP_Movement_Script.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class GP_Movement_Script : MonoBehaviour
+{
+    public float Progress = 0;
+    public float Max = 40;
+    public float progressSpeed = 1f; // how fast Progress increases per second
+    private bool nextStage = false;
+
+    public Slider slider;
+
+    public InputActionAsset inputActionAsset;
+    private InputAction leftHipAbducted;
+
+    private Coroutine progressRoutine;
+
+    void Awake()
+    {
+        var footMap = inputActionAsset.FindActionMap("Foot");
+        if (footMap == null)
+        {
+            Debug.LogWarning("Pottery: Foot map not found!");
+            return;
+        }
+        Debug.Log("Pottery: Foot map found!");
+
+        leftHipAbducted = footMap.FindAction("LeftHipAbducted");
+        if (leftHipAbducted == null)
+            Debug.LogWarning("Pottery: Left Hip Abducted not found!");
+        else
+            Debug.Log("Pottery: Left Hip Abducted found!");
+    }
+
+    void OnEnable()
+    {
+        if (leftHipAbducted == null) return;
+
+        leftHipAbducted.Enable();
+        leftHipAbducted.started += OnLeftHipStarted;
+        leftHipAbducted.canceled += OnLeftHipCanceled;
+
+        Debug.Log("Pottery: OnEnable");
+    }
+
+    void OnDisable()
+    {
+        if (leftHipAbducted == null) return;
+
+        leftHipAbducted.Disable();
+        leftHipAbducted.started -= OnLeftHipStarted;
+        leftHipAbducted.canceled -= OnLeftHipCanceled;
+
+        Debug.Log("Pottery: OnDisable");
+    }
+
+    void OnLeftHipStarted(InputAction.CallbackContext context)
+    {
+        Debug.Log("Pottery: Left Hip started (active)");
+        if (progressRoutine == null)
+            progressRoutine = StartCoroutine(AddProgressWhileActive());
+    }
+
+    void OnLeftHipCanceled(InputAction.CallbackContext context)
+    {
+        Debug.Log("Pottery: Left Hip canceled (released)");
+        if (progressRoutine != null)
+        {
+            StopCoroutine(progressRoutine);
+            progressRoutine = null;
+        }
+    }
+
+    IEnumerator AddProgressWhileActive()
+    {
+        while (leftHipAbducted.ReadValue<float>() > 0) // assumes float control (e.g., analog)
+        {
+            Progress += progressSpeed * Time.deltaTime;
+            slider.value = Progress;
+
+            if (Progress >= Max && !nextStage)
+            {
+                nextStage = true;
+                Debug.Log("Slider Full, nextStage = true");
+            }
+
+            yield return null;
+        }
+
+        progressRoutine = null;
+    }
+}
