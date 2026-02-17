3f17a1be49071c2b6077d6a515259c8861d91518
Helena Bjeletich <98721667+hbjeletich@users.noreply.github.com>
2026-02-17 16:34:17 -0600

    fixed LogData to LogInput

--- Assets/Golf/Scripts/GolfControls.cs
+++ Assets/Golf/Scripts/GolfControls.cs
@@ -61,14 +61,14 @@ public class GolfControls : MonoBehaviour
         float leftFootY = leftFootHeightAction.ReadValue<Vector3>().y;
         float rightFootY = rightFootHeightAction.ReadValue<Vector3>().y;
         string dataStr = $"Foot: {(leftFootY > rightFootY ? "Left" : "Right")}";
-        DataLogger.Instance.LogData("FootRaise", dataStr);
+        DataLogger.Instance.LogInput("FootRaise", dataStr);
             
         hasFootRaise = true;
     }
 
     public void stopStrengthAssesment(InputAction.CallbackContext ctx){
         float averageFootHeight = footHeightSamples > 0 ? totalFootHeight / footHeightSamples : 0f;
-        DataLogger.Instance.LogData("FootLower", $"AverageFootHeight: {averageFootHeight.ToString("F2")}; TimeRaised: {(footHeightSamples * Time.deltaTime).ToString("F2")}s");
+        DataLogger.Instance.LogInput("FootLower", $"AverageFootHeight: {averageFootHeight.ToString("F2")}; TimeRaised: {(footHeightSamples * Time.deltaTime).ToString("F2")}s");
         hasFootRaise = false;
     }
        
